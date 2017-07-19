d004977e8f92dcf33f3ffb38e0bf4d5cf4acb6e4
L0ndra <illialubenets@yandex.ua>
2017-07-19 19:42:42 +0000

    add creadentials and token models

---
+++ BotFrameworkMiddleware/BotCredentials.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotFrameworkMiddleware
+{
+    public class BotCredentials
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+
+    public class BotTokenResponse
+    {
+        public string access_token { get; set; }
+    }
+}
